c424a85b8582bfd265fc6cdebf8a8a2f0ad549b4
gokulkirubanandan <gokugokul31020@gmail.com>
2026-04-19 07:25:39 +0530

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/notes-app/',   // ← your repo name here
+  base: '/notes-app/',  // ← must match your exact repo name
 })
